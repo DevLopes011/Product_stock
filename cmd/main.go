9f291c76c3a0f5094c020121b05fad806be220b2
DevLopes011 <joao.arl.99@gmail.com>
2025-06-30 21:08:38 -0300

    feat: start Crud Arquitetura preparada para terminar desenvolvimento do crud Criado docker-compose teste

---
+++ cmd/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"product_stock/controller"
+	"product_stock/db"
+	"product_stock/repository"
+	"product_stock/usecase"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	server := gin.Default()
+	dbConnection, err := db.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
+	ProductRepository := repository.NewProductRepository(dbConnection)
+
+	ProductUseCase := usecase.NewProductUsecase(ProductRepository)
+
+	productController := controller.NewProductController(ProductUseCase)
+
+	server.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	server.POST("/product", productController.CreateProduct)
+	server.GET("/products", productController.GetProducts)
+	server.GET("/product/:productId", productController.GetProductById)
+
+	server.Run(":8000")
+}
---
+++ usecase/product_usecase.go
@@ -0,0 +1,39 @@
+package usecase
+
+import (
+	"product_stock/model"
+	"product_stock/repository"
+)
+
+type ProductUsecase struct {
+	repository repository.ProductRepository
+}
+
+func NewProductUsecase(repo repository.ProductRepository) ProductUsecase {
+	return ProductUsecase{
+		repository: repo,
+	}
+}
+
+func (pu *ProductUsecase) CreateProduct(product model.Product) (model.Product, error) {
+	productId, err := pu.repository.CreateProduct(product)
+	if err != nil {
+		return model.Product{}, err
+	}
+	product.ID = productId
+
+	return product, nil
+}
+
+func (pu *ProductUsecase) GetProducts() ([]model.Product, error) {
+	return pu.repository.GetProducts()
+}
+
+func (pu *ProductUsecase) GetProductById(id_product int) (*model.Product, error) {
+
+	product, err := pu.repository.GetProductById(id_product)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
